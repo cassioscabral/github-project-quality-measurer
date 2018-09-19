68b2e9260deddad456f164006470ec51c51decba
Cassio Cabral <cassioscabral@gmail.com>
2018-09-19 20:52:52 -0300

    Add ProjectSearch component

---
+++ src/components/ProjectSearch.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="project-search">
+  <h1>Message: {{msg}}</h1>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ProjectSearch extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <ProjectSearch msg="My Project Search"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ProjectSearch from '@/components/ProjectSearch.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    ProjectSearch,
   },
 })
 export default class Home extends Vue {}
