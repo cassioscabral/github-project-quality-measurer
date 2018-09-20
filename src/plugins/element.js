a1abe313913637c23681c44b02adea4ab2ff79b4
Cassio Cabral <cassioscabral@gmail.com>
2018-09-19 23:58:59 -0300

    Add Element IO

---
+++ src/plugins/element.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import {
+  Button,
+  Input
+} from 'element-ui'
+
+import lang from 'element-ui/lib/locale/lang/pt-br'
+import locale from 'element-ui/lib/locale'
+
+locale.use(lang)
+
+Vue.use(Button)
+Vue.use(Input)
