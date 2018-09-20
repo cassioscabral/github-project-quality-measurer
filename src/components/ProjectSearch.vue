5a466be4008773746effca1d21b559c5eebb1908
Cassio Cabral <cassioscabral@gmail.com>
2018-09-19 23:59:49 -0300

    add search input for projects

--- src/components/ProjectSearch.vue
+++ src/components/ProjectSearch.vue
@@ -1,15 +1,31 @@
 <template>
 <div class="project-search">
-  <h1>Message: {{msg}}</h1>
+  <h1>Search for the project: {{input}}</h1>
+  <el-input
+    placeholder="Project name"
+    v-model="input"
+    prefix-icon="el-icon-search"
+    @keyup.enter.native="search"
+    clearable>
+  </el-input>
+
+  Project: {{project}}
 </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
 export default class ProjectSearch extends Vue {
-  @Prop() private msg!: string;
+  protected input = ''
+  protected project = {} // TODO it should be a Project object
+
+  @Prop() private msg!: string
+
+  public search() {
+    this.project = {name: 'test', stars: 1000}
+  }
 }
 </script>
 
